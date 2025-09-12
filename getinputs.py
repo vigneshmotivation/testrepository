6ce6765757068f0f71584ccfd45d4fde1503922f
vigneshkarthikeyanvk <vigneshkarthikeyan.g@zohocorp.com>
2025-09-12 14:27:27 +0530

    new file added

---
+++ getinputs.py
@@ -0,0 +1,8 @@
+import findingmax
+arraylen = input ('enter number of elements')
+array = []
+i = 0
+while (int(i)<int (arraylen)):
+    array.append ( int ( input (f"enter {i}th element") ) )
+    i = int(i)+1
+print (findingmax.findmaxno(array))    
